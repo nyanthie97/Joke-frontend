063b329553daef7abf72a34675d1835b729b880f
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-09-13 01:59:39 +0300

    Changes

--- src/Components/Joke.js
+++ src/Components/Joke.js
@@ -3,3 +3,9 @@ import Punchline from './Punchline';
 import { getJoke } from './APICalls';
 import { JokeSetup } from './JokeSetup';
 import { Button } from 'reactstrap'
+
+export function Joke() {
+    const [jokeLoaded, setJokeLoaded] = useState(false);
+    const [theJoke, setTheJoke] = useState({});
+    const [showPunchline, setShowPunchline] = useState(false);
+    const [error, setError] = useState(null);
