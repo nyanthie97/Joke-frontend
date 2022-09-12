7e02d7159b13a36c7025afd7786b62724ae9771b
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-09-13 02:06:48 +0300

    update

--- src/Components/JokeSetup.js
+++ src/Components/JokeSetup.js
@@ -8,3 +8,21 @@ export function JokeSetup({ jokeLoaded, showPunchline, jokeType, setup, handlePu
   // once the tell me button has been clicked, be sure to hide it.
   // don't want to click again
   // could create components for showMeJoke and showTellMeButton
+
+
+  return (
+    <Card body inverse style={{ backgroundColor: '#85144b', borderColor: '#85144b' }}>
+      {jokeLoaded ?
+        <span>
+          <CardTitle>Joke Category: {firstLetterCase(jokeType)}</CardTitle>
+          <CardText>{setup}</CardText>
+        </span>
+        :
+        <CardTitle>Getting a Joke</CardTitle>}
+      {showPunchline ?
+        <div> </div>
+        : <div><Button color="info" onClick={handlePunchline}>TELL ME</Button></div>
+      }
+    </Card>
+  )
+}
