ed46b009bfd914b59de1f1c93b4830194754daf5
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-09-13 01:58:29 +0300

    Changes

---
+++ src/Components/Joke.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import Punchline from './Punchline';
+import { getJoke } from './APICalls';
+import { JokeSetup } from './JokeSetup';
+import { Button } from 'reactstrap'
