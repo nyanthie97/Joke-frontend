ec65b67eef5d0a29d4db3eb79e1c800a486f20a6
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-09-13 01:34:13 +0300

    update

---
+++ src/Components/APICalls.js
@@ -0,0 +1,15 @@
+function CheckError(response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw Error(response.statusText);
+    }
+  }
+  
+  
+  export const getJoke = () => {
+    return fetch("https://v2.jokeapi.dev/joke/Any?type=twopart&safe-mode")
+      .then(CheckError)
+      .then(response => response)
+  
+  }
