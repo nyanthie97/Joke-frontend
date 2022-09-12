b16f90988175d807bf6ed03d9dceb20965f9c7ad
Nyanthie Mayang <nyanthie.mayang@student.moringaschool.com>
2022-09-13 01:55:14 +0300

    Changes

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import { Joke } from './Joke';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header title="Tell Me Joke" />
+      <Joke />
+    </div>
+  );
+}
